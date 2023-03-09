5da8d83f3cc36995625509f71c5ebefbd535d884
mapbobi-commit <69840883+mapbobi-commit@users.noreply.github.com>
2023-03-09 19:13:22 +0900

    Add files via upload

---
+++ sentiment_analysis_gcloud.py
@@ -0,0 +1,139 @@
+from google.cloud import language_v1
+import six
+import pandas as pd
+from datetime import datetime
+import time
+import numpy as np
+
+
+# gcloud sentiment analysis function
+def sample_analyze_sentiment(content):
+    client = language_v1.LanguageServiceClient()
+
+    if isinstance(content, six.binary_type):
+        content = content.decode("utf-8")
+
+    type_ = language_v1.Document.Type.HTML
+    document = {"type_": type_, "content": content}
+
+    encoding_type = language_v1.EncodingType.UTF8
+    response = client.analyze_sentiment(
+        request={
+            "document": document,
+            "encoding_type": encoding_type,
+        }
+    )
+    return response
+
+
+def date_time_changer(date):
+    date_datetime = datetime.strptime(
+        str(date).replace("PDT", ""), "%a %b %d %H:%M:%S %Y"
+    )
+    return date_datetime.strftime("%Y-%m-%d")
+
+
+def gcloud_execution(number_of_randow_rows, table):
+    start_execution_time = time.time()
+
+    # Reads random rows
+    random_row_table = table.sample(number_of_randow_rows)
+    length_text_each_person = []
+    overall_text = ""
+
+    # Takes each random row and cleans it and then concatenates to the next row to create a single string
+    for row in random_row_table.index:
+        if (
+            random_row_table["text"][row]
+            .strip(" ")
+            .endswith("?")
+            or random_row_table["text"][row]
+            .strip(" ")
+            .endswith(".")
+            or random_row_table["text"][row]
+            .strip(" ")
+            .endswith("!")
+        ):
+            overall_text = "{}{} <br>".format(
+                overall_text,
+                random_row_table["text"][row].strip(" "),
+            )
+            length_text_each_person.append(
+                len(
+                    random_row_table["text"][row].strip(" ")
+                )
+            )
+        else:
+            overall_text = "{}{}.<br>".format(
+                overall_text,
+                random_row_table["text"][row].strip(" "),
+            )
+            length_text_each_person.append(
+                len(
+                    random_row_table["text"][row].strip(" ")
+                )
+            )
+
+    sentences_sentiment = sample_analyze_sentiment(
+        overall_text
+    ).sentences
+
+    # Puts the sentiment of every person into array
+    # If the text of a person is split into multiple sentences,
+    # it adds them up in the sentiment magnitude array and does the average of sentiment score
+    previous_offset = 0
+    i = 0
+    sentiment_score = []
+    sentiment_magnitude = []
+    for x in sentences_sentiment:
+        if len(sentiment_magnitude) == 0:
+            sentiment_magnitude.append(
+                x.sentiment.magnitude
+            )
+            sentiment_score.append([x.sentiment.score])
+        elif (
+            x.text.begin_offset - previous_offset
+            <= length_text_each_person[i]
+        ):
+            sentiment_magnitude[i] += x.sentiment.magnitude
+            sentiment_score[i].append(x.sentiment.score)
+        else:
+            previous_offset = x.text.begin_offset
+            sentiment_magnitude.append(
+                x.sentiment.magnitude
+            )
+            sentiment_score[i] = np.mean(sentiment_score[i])
+            sentiment_score.append([x.sentiment.score])
+            i += 1
+    sentiment_score[i] = np.mean(sentiment_score[i])
+
+    # Puts the random_row_table in a csv file
+    # Rounds the floats and changes the date format
+    (
+        random_row_table["sentiment_magnitude"],
+        random_row_table["sentiment_score"],
+    ) = (
+        sentiment_magnitude,
+        sentiment_score,
+    )
+    random_row_table["date"] = random_row_table[
+        "date"
+    ].apply(date_time_changer)
+    random_row_table[
+        "sentiment_magnitude"
+    ] = random_row_table["sentiment_magnitude"].apply(
+        round, [1]
+    )
+    random_row_table["sentiment_score"] = random_row_table[
+        "sentiment_score"
+    ].apply(round, [1])
+
+    # Writes it in a file
+    random_row_table.to_csv(
+        "sentiment_analysis_file_twit.csv", index=False
+    )
+
+    # Looks how much time the operation took
+    end_execution_time = time.time()
+    total_time = end_execution_time - start_execution_time
+    print("\n" + str(total_time))
---
+++ sentiment_analysis_prompt.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sentiment_analysis_gcloud import gcloud_execution
+from sentiment_analysis_vader import vader_execution
+
+
+# Takes the date and text columns
+data = pd.read_csv(
+    "training_data_sentiment_analysis.csv",
+    usecols=[2, 5],
+    encoding="latin-1",
+)
+
+
+add = False
+while add == False:
+    number_random_rows = int(
+        input("How many rows do you want in your csv file?")
+    )
+    vader_glcoud = input(
+        "Do you want to to run sentiment analysis with vader or gcloud( press 1 for vader or 2 for gcloud)"
+    )
+    if vader_glcoud == "1":
+        vader_execution(number_random_rows, data)
+    else:
+        gcloud_execution(number_random_rows, data)
+
+    something_add = input(
+        "Do you want to do something else (type 1 if you wish to make another table and anything else if you want to stop)"
+    )
+    if something_add != "1":
+        add = True
