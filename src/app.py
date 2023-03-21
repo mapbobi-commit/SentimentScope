c1456888c209c6681ed2d2217e39915ca5c4bb43
mapbobi-commit <69840883+mapbobi-commit@users.noreply.github.com>
2023-03-21 11:02:01 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,31 +1,50 @@
 import pandas as pd
-from sentiment_analysis_gcloud import gcloud_execution
-from sentiment_analysis_vader import vader_execution
+from gcloud import gcloud_execution
+from vader import vader_execution
 from utils import config
-import generator as gen
+import datetime
+
+
+def date_time_changer(date):
+    date_datetime = datetime.strptime(
+        str(date).replace("PDT", ""), "%a %b %d %H:%M:%S %Y"
+    )
+    return date_datetime.strftime("%Y-%m-%d")
+
+
+def write(method, table):
+    if method == "gcloud":
+        table.to_csv(
+            "sentiment_analysis_file.gcloud.csv",
+            index=False,
+        )
+    else:
+        table.to_csv(
+            "sentiment_analysis_file.vader.csv", index=False
+        )
+
 
 number_rows = config["rows"]
-method = config["method"]
+method_analysis = config["method"]
 number_of_tables = config["number_of_tables"]
 path_to_file = config["path_to_file"]
-
 
 # Takes the date and text columns
 data = pd.read_csv(
     path_to_file,
-    usecols=[2, 5],
+    usecols=[0, 1],
     encoding="latin-1",
 )
-for i in range(number_of_tables):
-    if method == "gcloud":
-        table = vader_execution(number_rows, data)
-        table["date"] = table["date"].apply(
-            gen.date_time_changer
-        )
-        gen.write(table)
-    else:
-        table = gcloud_execution(number_rows, data)
-        table["date"] = table["date"].apply(
-            gen.date_time_changer
-        )
-        gen.write(table)
+
+if method_analysis == "gcloud":
+    table = vader_execution(number_rows, data)
+    table["date"] = table["date"].apply(
+        date_time_changer
+    )
+    write(method_analysis, table)
+else:
+    table = gcloud_execution(number_rows, data)
+    table["date"] = table["date"].apply(
+        date_time_changer
+    )
+    write(method_analysis, table)
