1b73b38c5436c70bb91153e10c4b90df879980eb
mapbobi-commit <69840883+mapbobi-commit@users.noreply.github.com>
2023-03-23 19:27:54 +0900

    small changes to everything

--- src/vader.py
+++ src/vader.py
@@ -1,19 +1,21 @@
 from vaderSentiment.vaderSentiment import (
-    SentimentIntensityAnalyzer,
-)
+    SentimentIntensityAnalyzer, )
 import pandas as pd
 from datetime import datetime
 import time
 import numpy as np
 
+
+def date_time_changer(date):
+    date_datetime = datetime.strptime(
+        str(date).replace("PDT", ""), "%a %b %d %H:%M:%S %Y")
+    return date_datetime.strftime("%Y-%m-%d")
+
+
 # Function which rounds and gets the sentiment out of a string
-
-
-def sentiment_compound(x, analyzer, key="compound"):
+def sentiment_compound(x):
     return round(
-        SentimentIntensityAnalyzer().polarity_scores(x)[
-            key
-        ],
+        SentimentIntensityAnalyzer().polarity_scores(x)["compound"],
         1,
     )
 
@@ -22,12 +24,9 @@ def vader_execution(number_random_rows, table):
     # Takes random rows and creates a new column by applying sentiment_analyisis on the text of each person
     random_row_table = table.sample(number_random_rows)
 
-    random_row_table["sentiment_score"],
-    random_row_table["date"] = (
-        random_row_table["text"].apply(
-            sentiment_compound,
-            args=[SentimentIntensityAnalyzer, "compound"],
-        ),
-    )
+    random_row_table["sentiment_score"] = random_row_table["text"].apply(
+        sentiment_compound)
 
+    random_row_table["date"] = random_row_table["date"].apply(
+        date_time_changer)
     return random_row_table
