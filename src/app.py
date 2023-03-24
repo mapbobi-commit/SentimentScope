81d571482244c0eb9f58d4e763bb6de92a5d851b
mapbobi-commit <69840883+mapbobi-commit@users.noreply.github.com>
2023-03-24 22:06:48 +0900

    added sqlite3 support

--- src/app.py
+++ src/app.py
@@ -1,47 +1,40 @@
 import pandas as pd
 from gcloud import gcloud_execution
 from vader import vader_execution
-from utils import config
+import utils
 import datetime
 import random
 
-
-def write(method, table, csv_name):
-    if method == "gcloud":
-        table.to_csv(
-            csv_name + ".csv",
-            index=False,
-        )
-    else:
-        table.to_csv(csv_name + ".csv", index=False)
-
-
-path_to_file = config["path_to_file"]
-
 # Takes the date and text columns
-data = pd.read_csv(
-    path_to_file,
-    usecols=[0, 1],
-    encoding="latin-1",
+number_rows = int(
+    input("How many rows do you want the table to have?")
 )
-
-number_rows = int(input("How many rows do you want the table to have?"))
-csv_file_name = input("Path to the csv file you want to create."
-                      ) or "../data/nameless_csv_file" + str(
-                          random.randint(0, 100))
+csv_file_name = (
+    input(
+        "What path do you want to the csv file you want to create?"
+    )
+    or f"../data/nameless_csv_file {random.randint(0, 100)}"
+)
 method_analysis = input(
     'What method would you like to use for sentiment analysis (type "gcloud" or "vader")?'
 )
+path_to_file = (
+    input(
+        "What's the path to the file you want to use (either csv or sqlite or db)?"
+    )
+    or "..data/example_csv/example_csv_file.csv"
+)
 
+data = utils.read(path_to_file)
 problem = True
 while problem:
     if method_analysis == "vader":
         table = vader_execution(number_rows, data)
-        write(method_analysis, table, csv_file_name)
+        utils.write(table, csv_file_name, path_to_file)
         problem = False
     elif method_analysis == "gcloud":
         table = gcloud_execution(number_rows, data)
-        write(method_analysis, table, csv_file_name)
+        utils.write(table, csv_file_name, path_to_file)
         problem = False
     else:
         print(
